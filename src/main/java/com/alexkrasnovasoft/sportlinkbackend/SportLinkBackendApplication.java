5ba168232e3e91ef26ca035bc32b2e36850b147d
Alexandra Krasnova <krasnova@physics.msu.ru>
2024-04-19 13:16:37 +0200

    Initial commit.

---
+++ src/main/java/com/alexkrasnovasoft/sportlinkbackend/SportLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alexkrasnovasoft.sportlinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportLinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportLinkBackendApplication.class, args);
+	}
+
+}
