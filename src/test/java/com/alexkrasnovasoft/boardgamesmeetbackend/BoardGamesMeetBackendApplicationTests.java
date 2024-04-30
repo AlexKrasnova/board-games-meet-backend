676ed4b5c7a9f714c02fa6063fb89851a618e983
Alexandra Krasnova <krasnova@physics.msu.ru>
2024-04-30 23:41:03 +0200

    Renamed the project.

---
+++ src/test/java/com/alexkrasnovasoft/boardgamesmeetbackend/BoardGamesMeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexkrasnovasoft.boardgamesmeetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGamesMeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
